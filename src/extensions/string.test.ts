14f78a68c24edf86d9834e64e271e7fb4fb7b85a
Alexandr Rodik <rodik@readdle.com>
2017-03-21 23:06:27 +0200

    test: basic test cases for string extension

---
+++ src/extensions/string.test.ts
@@ -0,0 +1,49 @@
+import StringExtension from "./string";
+
+describe("getWords", function() {
+    test("different separator formats", function() {
+        const expectedResult = ["test", "test1", "test2"];
+        const testSet = [
+            "test test1 test2",
+            "test.test1.test2",
+            "test_test1_test2",
+            "test-test1-test2",
+            "test test1.test2",
+            "test-test1_test2",
+            "test_test1.test2"
+        ];
+
+        testSet.forEach(function(testString) {
+            expect(StringExtension.getWords(testString))
+                .toEqual(expectedResult);
+        });
+    });
+
+    test("different separator formats, first char of each word is uppercased", function() {
+        const expectedResult = ["Test", "Test1", "Test2"];
+        const testSet = [
+            "Test-Test1-Test2",
+            "Test.Test1.Test2",
+            "Test_Test1_Test2",
+            "Test_Test1.Test2",
+        ];
+
+        testSet.forEach(function(testString) {
+            expect(StringExtension.getWords(testString))
+                .toEqual(expectedResult);
+        });
+    });
+
+    test("uppercase and numbers", function() {
+        const testSet = [
+            {str:"TestTest1Test2", result: ["Test", "Test", "1", "Test2"]},
+            {str:"Test-Test1Test2", result: ["Test", "Test", "1", "Test2"]},
+            {str:"TestTest1-Test2", result: ["Test", "Test1", "Test2"]}
+        ];
+
+        testSet.forEach(function(testCase) {
+            expect(StringExtension.getWords(testCase.str))
+                .toEqual(testCase.result);
+        });
+    });
+});
--- src/extensions/string.ts
+++ src/extensions/string.ts
@@ -1,7 +1,11 @@
-class StringExtsnsion {
+export default class StringExtension {
 
+    /**
+     *  Get words from
+     *  PascalCase, camelCase, kebab-case, Train-Case, snake_case, dot.case
+     */
     static getWords(target: string): Array<string> {
-
+        return target.split(/[\s\-._]+/);
     }
 
     static ucfirst(target: string): string {
@@ -9,46 +13,44 @@ class StringExtsnsion {
     }
 
     static lowercaseSeparate(target: string, separator: string): string {
-        return StringExtsnsion.getWords(target)
+        return StringExtension.getWords(target)
             .map(element => element.toLowerCase())
             .join(separator);
     }
 
     static camelCase(target: string): string {
-        return StringExtsnsion.getWords(target).reduce(function(buffer, element, index) {
+        return StringExtension.getWords(target).reduce(function(buffer, element, index) {
             const isFirstPart = index === 0;
             const namePart = isFirstPart
                 ? element.toLowerCase()
-                : StringExtsnsion.ucfirst(element.toLowerCase());
+                : StringExtension.ucfirst(element.toLowerCase());
 
             return buffer + namePart;
         }, "");
     }
 
     static pascalCase(target: string): string {
-        return StringExtsnsion.ucfirst(
-            StringExtsnsion.camelCase(target)
+        return StringExtension.ucfirst(
+            StringExtension.camelCase(target)
         )
     }
 
     static kebabCase(target: string): string {
-        return StringExtsnsion.lowercaseSeparate(target, "-");
+        return StringExtension.lowercaseSeparate(target, "-");
     }
 
     static trainCase(target: string): string {
-        return StringExtsnsion.getWords(target)
-            .map(element => StringExtsnsion.ucfirst(element.toLowerCase()))
+        return StringExtension.getWords(target)
+            .map(element => StringExtension.ucfirst(element.toLowerCase()))
             .join("-");
     }
 
     static snakeCase(target: string): string {
-        return StringExtsnsion.lowercaseSeparate(target, "_");
+        return StringExtension.lowercaseSeparate(target, "_");
     }
 
     static dotCase(target: string): string {
-        return StringExtsnsion.lowercaseSeparate(target, ".");
+        return StringExtension.lowercaseSeparate(target, ".");
     }
 
 }
-
-module.exports = StringExtsnsion;
